b3ceeee38995c8125b13fba06d5fab5554d0bb62
Rakib Hasan Babu <rhbabu3@gmail.com>
2023-11-16 19:31:00 +0600

    client session and server session updated

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import "../styles/tailwind.css";
 
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import AuthProvider from "./(components)/AuthProvider";
 
 export const metadata = {
     title: "Next JS | Next Auth | RTK",
@@ -11,15 +12,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className="bg-zinc-50" suppressHydrationWarning={true}>
-                <div className="mx-2 md:mx-0 sticky top-8 z-50">
-                    <Header />
-                </div>
-                <main className="container mx-auto max-w-5xl p-2 sm:p-4 md:p-6 xl:p-8">
-                    <div className="my-20 min-h-[50vh]">{children}</div>
-                </main>
-                <Footer />
-            </body>
+            <AuthProvider>
+                <body className="bg-zinc-50" suppressHydrationWarning={true}>
+                    <div className="mx-2 md:mx-0 sticky top-8 z-50">
+                        <Header />
+                    </div>
+                    <main className="container mx-auto max-w-5xl p-2 sm:p-4 md:p-6 xl:p-8">
+                        <div className="my-20 min-h-[50vh]">{children}</div>
+                    </main>
+                    <Footer />
+                </body>
+            </AuthProvider>
         </html>
     );
 }
--- src/app/server-member/page.jsx
+++ src/app/server-member/page.jsx
@@ -1,15 +1,35 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { options } from "../api/auth/[...nextauth]/options";
+
 export const metadata = {
     title: "Server Member",
     description: "Server side rendering",
 };
 
-export default function ServerMember() {
+export default async function ServerMember() {
+    const session = await getServerSession(options);
+
+    if (!session) {
+        redirect("/api/auth/signin?callbackUrl=/server-member");
+    }
+
     return (
         <section>
             <div className="space-y-2">
                 <h3 className="text-lg font-semibold">Server</h3>
 
                 <p>Server side rendering!</p>
+
+                <div>
+                    <p className="font-semibold mb-2">
+                        You are seeing this page, cause you have authorized
+                        yourself!
+                    </p>
+                    <pre>{session?.user?.email}</pre>
+                    <pre>{session?.user?.name}</pre>
+                    <pre>{session?.user?.role}</pre>
+                </div>
             </div>
         </section>
     );
